196627d95befe719c67ae6576f9895a50cc5cd73
Kamal <kamal@eidolonsystems.com>
2017-11-17 18:55:00 -0500

    Added column width support.

--- source/table_view.tsx
+++ source/table_view.tsx
@@ -34,6 +34,10 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
       height: this.props.viewHeight,
       overflow: 'auto' as 'auto'
     };
+    this.columnWidths = [];
+    for(let i = 0; i < this.props.model.columnCount; ++i) {
+      this.columnWidths.push(0);
+    }
   }
 
   public render(): JSX.Element {
@@ -43,13 +47,6 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
              style={this.divStyle}>
         </div>);
     }
-    let rows: Array<JSX.Element> = [];
-    let headers: Array<JSX.Element> = [];
-    for(let columnIndex = 0; columnIndex < this.props.model.columnCount;
-        ++columnIndex) {
-      headers.push(<th>{this.props.model.getName(columnIndex)}</th>);
-    }
-    rows.push(<tr>{headers}</tr>);
     let rowHeight = 0;
     let topRow = 0;
     let bottomRow = 0;
@@ -58,6 +55,13 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
         this.container, null).getPropertyValue('font-size');
       rowHeight = Math.floor(parseFloat(style));
       bottomRow = Math.min(1, this.props.model.rowCount);
+      let viewWidth = Math.floor(parseFloat(window.getComputedStyle(
+        this.container, null).getPropertyValue('width')));
+      let initialColumnWidth = Math.floor
+        (viewWidth / this.props.model.columnCount);
+      for(let i = 0; i < this.props.model.columnCount; ++i) {
+        this.columnWidths[i] = initialColumnWidth;
+      }
     } else {
       rowHeight = this.table.rows[0].offsetHeight;
       let viewHeight = Math.floor(parseFloat(window.getComputedStyle(
@@ -67,6 +71,21 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
       bottomRow = Math.min(topRow + displayedRowCount,
         this.props.model.rowCount);
     }
+    let columns: Array<JSX.Element> = [];
+    let headers: Array<JSX.Element> = [];
+    for(let columnIndex = 0; columnIndex < this.props.model.columnCount;
+        ++columnIndex) {
+      let columnStyle = {
+        width: `${this.columnWidths[columnIndex]}px`
+      };
+      columns.push(<col style={columnStyle} />);
+      headers.push(<th>{this.props.model.getName(columnIndex)}</th>);
+    }
+    let rows: Array<JSX.Element> = [];
+    rows.push.apply(rows, columns);
+    rows.push(<thead onMouseMove={this.onMouseMoveTableHeader.bind(this)}>
+                <tr>{headers}</tr>
+              </thead>);
     for(let rowIndex = topRow; rowIndex < bottomRow; ++rowIndex) {
       let row: Array<JSX.Element> = [];
       for(let columnIndex = 0; columnIndex < this.props.model.columnCount;
@@ -84,6 +103,25 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
            style={this.divStyle}
            onScroll={this.onScroll.bind(this)}>
         <div style={innerStyle}>
+          <style scoped>{`
+            table {
+              border-collapse: collapse;
+              -webkit-user-select: none;
+              -khtml-user-select: none;
+              -moz-user-select: none;
+              -ms-user-select: none;
+              -o-user-select: none;
+              user-select: none;
+              cursor: default;
+            }
+            table, th {
+              border:1px solid black;
+            }
+            td {
+              text-align: center;
+              vertical-align: middle;
+            }`}
+          </style>
           <table ref={(element) => {this.table = element;}}>
             {rows}
           </table>
@@ -121,7 +159,30 @@ class TableView extends React.Component<TableViewProperties, TableViewState> {
       });
   }
 
+  private onMouseMoveTableHeader(event: MouseEvent): void {
+    if(this.columnWidths.length == 0) {
+      return;
+    }
+    const RESIZE_RANGE = 5;
+    let columnSelected = -1;
+    for(let i = 0; i < this.props.model.columnCount; ++i) {
+      let element = document.elementFromPoint(event.clientX, event.clientY) as
+        HTMLElement;
+      let elementLeft = element.getBoundingClientRect().left;
+      let elementRight = element.getBoundingClientRect().right;
+      if(event.clientX >= elementLeft &&
+          event.clientX <= elementLeft + RESIZE_RANGE ||
+          event.clientX >= elementRight - RESIZE_RANGE) {
+        element.style.cursor = 'ew-resize';
+        return;
+      } else {
+        element.style.cursor = 'default';
+      }
+    }
+  }
+
   private divStyle: any;
+  private columnWidths: Array<number>;
   private container: HTMLDivElement;
   private table: HTMLTableElement;
 }
